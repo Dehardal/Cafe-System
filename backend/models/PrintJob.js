22e58ea0824ab1208a9abf3d0635ae1a167cebd8
Dehardal <ddayal7143@gmail.com>
2026-05-16 18:53:28 +0530

    🚀 DATABASE: Made phone number optional

--- backend/models/PrintJob.js
+++ backend/models/PrintJob.js
@@ -12,7 +12,7 @@ const printJobSchema = mongoose.Schema({
     },
     phone: {
         type: String,
-        required: true,
+        required: false,
     },
     fileUrl: {
         type: String,
